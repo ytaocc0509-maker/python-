fd451a7f3f42dcbac96c92b48a5625daee988889
ytaocc0509-maker <ytaocc0509@gmail.com>
2026-03-10 22:33:16 +0800

    Add files via upload

---
+++ python0310.py
@@ -0,0 +1,22 @@
+# # import test_package.module01
+# # import test_package.module02
+# # from test_package.module01 import hi
+# # from test_package import *
+# from test_package.test_package02 import module03
+#
+# # from test_package import module02
+#
+# # test_package.module01.hi()
+# # module02.ok()
+# # module01.hi()
+#
+# # import test_package.test_package02.module03
+# #
+# # test_package.test_package02.module03.hello()
+#
+# # from test_package.test_package02.module03 import hello
+# #
+# # hello()
+#
+# # from test_package.test_package02 import  module03
+# module03.hello()
